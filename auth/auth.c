ykpmusicstudio/xone

commit 87f538ca0c31f81ed333e7e898b6ed4d1cc03d9c

Move buffer allocation from auth to crypto

auth/auth.c
@@ -257,9 +257,11 @@ static int gip_auth2_handle_pkt_pubkey(struct gip_auth *auth,
 	return 0;
 }
 
-static int gip_auth2_compute_master_secret(struct gip_auth *auth,
-					   u8 *pubkey, int len)
+static void gip_auth2_exchange_ecdh(struct work_struct *work)
 {
+	struct gip_auth *auth = container_of(work, typeof(*auth),
+					     work_exchange_ecdh);
+	struct gip_auth2_pkt_host_pubkey pkt = {};
 	u8 random[GIP_AUTH_RANDOM_LEN * 2];
 	u8 secret[GIP_AUTH2_SECRET_LEN];
 	int err;
@@ -268,45 +270,30 @@ static int gip_auth2_compute_master_secret(struct gip_auth *auth,
 	memcpy(random + sizeof(auth->random_host), auth->random_client,
 	       sizeof(auth->random_client));
 
-	err = gip_auth_compute_ecdh(auth->pubkey_client2, pubkey,
-				    len, secret);
-	if (err)
-		return err;
-
-	return gip_auth_compute_prf(auth->shash_prf, "Master Secret",
-				    secret, sizeof(secret),
-				    random, sizeof(random),
-				    auth->master_secret,
-				    sizeof(auth->master_secret));
-}
-
-static void gip_auth2_exchange_ecdh(struct work_struct *work)
-{
-	struct gip_auth *auth = container_of(work, typeof(*auth),
-					     work_exchange_ecdh);
-	struct gip_auth2_pkt_host_pubkey *pkt;
-	int err;
-
-	pkt = kzalloc(sizeof(*pkt), GFP_KERNEL);
-	if (!pkt)
-		return;
-
-	err = gip_auth2_compute_master_secret(auth, pkt->pubkey,
-					      sizeof(pkt->pubkey));
+	err = gip_auth_compute_ecdh(auth->pubkey_client2, pkt.pubkey,
+				    sizeof(pkt.pubkey), secret);
 	if (err) {
-		dev_err(&auth->client->dev, "%s: compute secret failed\n",
-			__func__);
-		goto err_free_pkt;
+		dev_err(&auth->client->dev, "%s: compute ECDH failed: %d\n",
+			__func__, err);
+		return;
+	}
+
+	err = gip_auth_compute_prf(auth->shash_prf, "Master Secret",
+				   secret, sizeof(secret),
+				   random, sizeof(random),
+				   auth->master_secret,
+				   sizeof(auth->master_secret));
+	if (err) {
+		dev_err(&auth->client->dev, "%s: compute PRF failed: %d\n",
+			__func__, err);
+		return;
 	}
 
 	err = gip_auth_send_pkt(auth, GIP_AUTH2_CMD_HOST_PUBKEY,
-				pkt, sizeof(*pkt));
+				&pkt, sizeof(pkt));
 	if (err)
 		dev_err(&auth->client->dev, "%s: send pkt failed: %d\n",
 			__func__, err);
-
-err_free_pkt:
-	kfree(pkt);
 }
 
 static int gip_auth_send_pkt_hello(struct gip_auth *auth)
@@ -457,68 +444,46 @@ static int gip_auth_handle_pkt_finish(struct gip_auth *auth,
 	return 0;
 }
 
-static int gip_auth_compute_master_secret(struct gip_auth *auth,
-					  u8 *encrypted_pms, int len)
+static void gip_auth_exchange_rsa(struct work_struct *work)
 {
+	struct gip_auth *auth = container_of(work, typeof(*auth),
+					     work_exchange_rsa);
+	struct gip_auth_pkt_host_secret pkt = {};
 	u8 random[GIP_AUTH_RANDOM_LEN * 2];
-	u8 *pms;
+	u8 pms[GIP_AUTH_SECRET_LEN];
 	int err;
 
 	memcpy(random, auth->random_host, sizeof(auth->random_host));
 	memcpy(random + sizeof(auth->random_host), auth->random_client,
 	       sizeof(auth->random_client));
 
-	pms = kmalloc(GIP_AUTH_SECRET_LEN, GFP_KERNEL);
-	if (!pms)
-		return -ENOMEM;
-
 	/* get random premaster secret */
 	get_random_bytes(pms, sizeof(pms));
 
 	err = gip_auth_encrypt_rsa(auth->pubkey_client, GIP_AUTH_PUBKEY_LEN,
-				   pms, GIP_AUTH_SECRET_LEN, encrypted_pms,
-				   len);
-	if (err)
-		goto err_free_pms;
+				   pms, GIP_AUTH_SECRET_LEN, pkt.encrypted_pms,
+				   sizeof(pkt.encrypted_pms));
+	if (err) {
+		dev_err(&auth->client->dev, "%s: encrypt RSA failed: %d\n",
+			__func__, err);
+		return;
+	}
 
 	err = gip_auth_compute_prf(auth->shash_prf, "Master Secret",
 				   pms, sizeof(pms), random, sizeof(random),
 				   auth->master_secret,
 				   sizeof(auth->master_secret));
-
-err_free_pms:
-	kfree(pms);
-
-	return err;
-}
-
-static void gip_auth_exchange_rsa(struct work_struct *work)
-{
-	struct gip_auth *auth = container_of(work, typeof(*auth),
-					     work_exchange_rsa);
-	struct gip_auth_pkt_host_secret *pkt;
-	int err;
-
-	pkt = kzalloc(sizeof(*pkt), GFP_KERNEL);
-	if (!pkt)
-		return;
-
-	err = gip_auth_compute_master_secret(auth, pkt->encrypted_pms,
-					     sizeof(pkt->encrypted_pms));
 	if (err) {
-		dev_err(&auth->client->dev, "%s: compute secret failed\n",
-			__func__);
-		goto err_free_pkt;
+		dev_err(&auth->client->dev, "%s: compute PRF failed: %d\n",
+			__func__, err);
+		return;
 	}
 
 	err = gip_auth_send_pkt(auth, GIP_AUTH_CMD_HOST_SECRET,
-				pkt, sizeof(*pkt));
+				&pkt, sizeof(pkt));
 	if (err)
 		dev_err(&auth->client->dev, "%s: send pkt failed: %d\n",
 			__func__, err);
-
-err_free_pkt:
-	kfree(pkt);
 }
 
 static void gip_auth_complete_handshake(struct work_struct *work)

auth/crypto.c
@@ -126,13 +126,13 @@ err_free_tfm:
 }
 
 static int gip_auth_ecdh_get_pubkey(struct crypto_kpp *tfm,
-				    u8 *pubkey, int len)
+				    u8 *out, int len)
 {
 	struct kpp_request *req;
 	struct scatterlist dest;
 	struct ecdh key = {};
 	DECLARE_CRYPTO_WAIT(wait);
-	void *privkey;
+	void *privkey, *pubkey;
 	unsigned int privkey_len;
 	int err;
 
@@ -141,19 +141,23 @@ static int gip_auth_ecdh_get_pubkey(struct crypto_kpp *tfm,
 	if (!privkey)
 		return -ENOMEM;
 
+	pubkey = kzalloc(len, GFP_KERNEL);
+	if (!pubkey)
+		goto err_free_privkey;
+
 	/* generate private key */
 	err = crypto_ecdh_encode_key(privkey, privkey_len, &key);
 	if (err)
-		goto err_free_privkey;
+		goto err_free_pubkey;
 
 	err = crypto_kpp_set_secret(tfm, privkey, privkey_len);
 	if (err)
-		goto err_free_privkey;
+		goto err_free_pubkey;
 
 	req = kpp_request_alloc(tfm, GFP_KERNEL);
 	if (!req) {
 		err = -ENOMEM;
-		goto err_free_privkey;
+		goto err_free_pubkey;
 	}
 
 	sg_init_one(&dest, pubkey, len);
@@ -163,11 +167,15 @@ static int gip_auth_ecdh_get_pubkey(struct crypto_kpp *tfm,
 	kpp_request_set_callback(req, CRYPTO_TFM_REQ_MAY_BACKLOG,
 				 crypto_req_done, &wait);
 	err = crypto_wait_req(crypto_kpp_generate_public_key(req), &wait);
+	if (!err)
+		memcpy(out, pubkey, len);
 
 	kpp_request_free(req);
 
+err_free_pubkey:
+	kfree(pubkey);
 err_free_privkey:
-	kfree_sensitive(privkey);
+	kfree(privkey);
 
 	return err;
 }
@@ -207,7 +215,7 @@ int gip_auth_compute_ecdh(u8 *pubkey_in, u8 *pubkey_out,
 	u8 *secret;
 	int err;
 
-	secret = kmalloc(GIP_AUTH_ECDH_SECRET_LEN, GFP_KERNEL);
+	secret = kzalloc(GIP_AUTH_ECDH_SECRET_LEN, GFP_KERNEL);
 	if (!secret)
 		return -ENOMEM;
 
@@ -237,10 +245,8 @@ int gip_auth_compute_ecdh(u8 *pubkey_in, u8 *pubkey_out,
 
 err_free_sha:
 	crypto_free_shash(tfm_sha);
-
 err_free_ecdh:
 	crypto_free_kpp(tfm_ecdh);
-
 err_free_secret:
 	kfree(secret);
 

commit ac1ce4c33a9e25af2f27fffe9dca60acb4705b1e

fix lockup caused by bug in auth
The pms buffer would trigger a BUG() when passed to sg_init_one()
because virt_addr_valid() returns false for stack addresses (if built
with CONFIG_VMAP_STACK=y). Fix it by moving the buffer to struct
gip_auth.

Fixes: 87f538ca0c31 ("Move buffer allocation from auth to crypto")

auth/auth.c
@@ -449,7 +449,6 @@ static void gip_auth_exchange_rsa(struct work_struct *work)
 					     work_exchange_rsa);
 	struct gip_auth_pkt_host_secret pkt = {};
 	u8 random[GIP_AUTH_RANDOM_LEN * 2];
-	u8 pms[GIP_AUTH_SECRET_LEN];
 	int err;
 
 	memcpy(random, auth->random_host, sizeof(auth->random_host));
@@ -457,11 +456,12 @@ static void gip_auth_exchange_rsa(struct work_struct *work)
 	       sizeof(auth->random_client));
 
 	/* get random premaster secret */
-	get_random_bytes(pms, sizeof(pms));
+	get_random_bytes(auth->pms, sizeof(auth->pms));
 
 	err = gip_auth_encrypt_rsa(auth->pubkey_client,
 				   sizeof(auth->pubkey_client),
-				   pms, sizeof(pms), pkt.encrypted_pms,
+				   auth->pms, sizeof(auth->pms),
+				   pkt.encrypted_pms,
 				   sizeof(pkt.encrypted_pms));
 	if (err) {
 		dev_err(&auth->client->dev, "%s: encrypt RSA failed: %d\n",
@@ -470,7 +470,8 @@ static void gip_auth_exchange_rsa(struct work_struct *work)
 	}
 
 	err = gip_auth_compute_prf(auth->shash_prf, "Master Secret",
-				   pms, sizeof(pms), random, sizeof(random),
+				   auth->pms, sizeof(auth->pms),
+				   random, sizeof(random),
 				   auth->master_secret,
 				   sizeof(auth->master_secret));
 	if (err) {

auth/auth.h
@@ -41,6 +41,7 @@ struct gip_auth {
 	u8 pubkey_client[GIP_AUTH_PUBKEY_LEN];
 	u8 pubkey_client2[GIP_AUTH2_PUBKEY_LEN];
 
+	u8 pms[GIP_AUTH_SECRET_LEN];
 	u8 master_secret[GIP_AUTH_SECRET_LEN];
 };
 